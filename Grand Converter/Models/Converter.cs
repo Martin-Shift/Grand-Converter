12aaa217bbfddc2f17855f5b0384fb4631518594
Martin Saprykin <sapr_dp83@student.itstep.net>
2023-01-22 20:20:32 +0100

    Code Polish

---
+++ Grand Converter/Models/Converter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Grand_Converter.Models
+{
+    public class ValueConverter
+    {
+        public ValueConverter() { }
+        public string From { get; set; } = "";
+        public string To { get; set; } = "";
+        public string Result { get; set; } = "";
+        public double Amount { get; set; } = 0;
+        public DateTime ConvertTime { get; set; } = DateTime.Now;
+        public string ForRate { get; set; } = "";
+        public string ToRate { get; set; } = "";
+        public static readonly List<string> Values = File.ReadAllText(@"D:\Mein progectos\Grand Converter\new.txt").Split('\n').ToList();
+        public double GetRate()
+        {
+            WebClient Client = new();
+            string url = $"https://fxds-public-exchange-rates-api.oanda.com/cc-api/currencies?base={From}&quote={To}&data_type=general_currency_pair&start_date={ConvertTime - new TimeSpan(1, 0, 0, 0):yyyy-MM-dd}&end_date={ConvertTime:yyyy-MM-dd}";
+            return Convert.ToDouble(Regex.Match(Client.DownloadString(url), @"average_bid.:.(\d+.\d+)").Groups[1].Value, CultureInfo.InvariantCulture);
+        }
+
+    }
+}
--- Grand Converter/ViewModels/MainWindowViewModel.cs
+++ Grand Converter/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,11 @@
-﻿using My.BaseViewModels;
+﻿using Grand_Converter.Models;
+using My.BaseViewModels;
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using System.Windows.Input;
 
@@ -13,21 +13,20 @@ namespace Grand_Converter.ViewModels
 {
     public class MainWindowViewModel : NotifyPropertyChangedBase
     {
+        private ValueConverter _converter;
         public MainWindowViewModel()
-        {
-            Strings = new ObservableCollection<string>(File.ReadAllText(@"D:\Mein progectos\Grand Converter\new.txt").Split('\n'));
+        { 
+            _converter = new();
         }
-        private DateTime _convertTime = DateTime.Now;
-        public DateTime ConvertTime { get => _convertTime; set { _convertTime = value; OnPropertyChanged(nameof(ConvertTime)); } }
-        public WebClient Client = new WebClient();
-        public ObservableCollection<string> FromFinds { get; set; } = new ObservableCollection<string>(File.ReadAllText(@"D:\Mein progectos\Grand Converter\new.txt").Split('\n'));
+        public ObservableCollection<string> FromFinds { get; set; } = new ObservableCollection<string>(ValueConverter.Values);
+        public ObservableCollection<string> ToFinds { get; set; } = new ObservableCollection<string>(ValueConverter.Values);
         private string _fromFind = "";
         public string FromFind
         {
             get => _fromFind; set
             {
                 _fromFind = value;
-                FromFinds = new ObservableCollection<string>(Strings.Where(x => x.Contains(FromFind.ToUpper())));
+                FromFinds = new ObservableCollection<string>(ValueConverter.Values.Where(x => x.Contains(FromFind.ToUpper())));
                 OnPropertyChanged(nameof(FromFind));
                 OnPropertyChanged(nameof(FromFinds));
             }
@@ -38,38 +37,52 @@ namespace Grand_Converter.ViewModels
             get => _toFind; set
             {
                 _toFind = value;
-                ToFinds = new ObservableCollection<string>(Strings.Where(x => x.Contains(ToFind.ToUpper())));
+                ToFinds = new ObservableCollection<string>(ValueConverter.Values.Where(x => x.Contains(ToFind.ToUpper())));
                 OnPropertyChanged(nameof(ToFind));
                 OnPropertyChanged(nameof(ToFinds));
             }
         }
-
-        public ObservableCollection<string> ToFinds { get; set; } = new ObservableCollection<string>(File.ReadAllText(@"D:\Mein progectos\Grand Converter\new.txt").Split('\n'));
-        public ObservableCollection<string> Strings { get; set; } = new ObservableCollection<string>(File.ReadAllText(@"D:\Mein progectos\Grand Converter\new.txt").Split('\n'));
-        private double _amount = 0;
+        public DateTime ConvertTime
+        {
+            get => _converter.ConvertTime;
+            set { _converter.ConvertTime = value; OnPropertyChanged(nameof(ConvertTime)); }
+        }
         public double Amount
         {
-            get => _amount;
-            set { _amount = value; OnPropertyChanged(nameof(Amount)); }
+            get => _converter.Amount;
+            set { _converter.Amount = value; OnPropertyChanged(nameof(Amount)); }
         }
-        private string _from = "";
-        public string From { get => _from; set { _from = value; OnPropertyChanged(nameof(From)); } }
-        private string _to = "";
-        public string To { get => _to; set { _to = value; OnPropertyChanged(nameof(To)); } }
-        private string _result = "";
-        public string Result { get => _result; set { _result = value; OnPropertyChanged(nameof(Result)); } }
-        private string _for1 = "";
-        public string For1 { get => _for1; set { _for1 = value; OnPropertyChanged(nameof(For1)); } }
-        private string _to1 = "";
-        public string To1 { get => _to1; set { _to1 = value; OnPropertyChanged(nameof(To1)); } }
+        public string From
+        {
+            get => _converter.From;
+            set { _converter.From = value; OnPropertyChanged(nameof(From)); }
+        }
+        public string To
+        {
+            get => _converter.To;
+            set { _converter.To = value; OnPropertyChanged(nameof(To)); }
+        }
+        public string Result
+        {
+            get => _converter.Result;
+            set { _converter.Result = value; OnPropertyChanged(nameof(Result)); }
+        }
+        public string For1
+        {
+            get => _converter.ForRate; set
+            {
+                _converter.ForRate = value;
+                OnPropertyChanged(nameof(For1));
+            }
+        }
+        public string To1 { get => _converter.ToRate; set { _converter.ToRate = value; OnPropertyChanged(nameof(To1)); } }
         public ICommand Calculate => new RelayCommand(
             x =>
-           { 
-               string url = $"https://fxds-public-exchange-rates-api.oanda.com/cc-api/currencies?base={From}&quote={To}&data_type=general_currency_pair&start_date={ConvertTime - new TimeSpan(1, 0, 0, 0):yyyy-MM-dd}&end_date={ConvertTime:yyyy-MM-dd}";
-               double res = Convert.ToDouble(Regex.Match(Client.DownloadString(url).Replace('.', ','), @"average_bid.:.(\d+.\d+)").Groups[1].Value);
-               For1 = $"1 {From} = {Math.Round(res,2)} {To}";
-               To1 = $"1 {To} = {Math.Round(1 / res, 2)} {From}";
-               Result = $"{Amount} {From} = {Math.Round(res * Amount,2)} {To}";
+           {
+               var rate = _converter.GetRate();
+               For1 = $"1 {From} = {Math.Round(rate, 2)} {To}";
+               To1 = $"1 {To} = {Math.Round(1 / rate, 2)} {From}";
+               Result = $"{Amount} {From} = {Math.Round(rate * Amount, 2)} {To}";
            }, x => From != "" && To != "" && Amount != 0);
         public ICommand Swap => new RelayCommand(
             x =>
@@ -78,8 +91,8 @@ namespace Grand_Converter.ViewModels
             string totemp = From;
             FromFind = "";
             ToFind = "";
-            From = Strings.Where(x => x.Contains(fromtemp.ToUpper())).First();
-            To = Strings.Where(x => x.Contains(totemp.ToUpper())).First();
+            From = ValueConverter.Values.Where(x => x.Contains(fromtemp.ToUpper())).First();
+            To = ValueConverter.Values.Where(x => x.Contains(totemp.ToUpper())).First();
         }, x => From != "" && To != ""
         );
     }
